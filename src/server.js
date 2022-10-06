ecfb8a0789b9b3b707a4f176f71dcf94e3ab5099
Maria Paula <79462753+mariabgbarros@users.noreply.github.com>
2022-10-06 10:33:45 -0300

    Removendo package-lock.json

--- src/server.js
+++ src/server.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(express.json())
 app.use(routes);
 
-app.listen(3333, () => {
-    console.log("Servidor rodando na porta 3333");
+const port = process.env.PORT || 3333;
+app.listen(port, () => {
+    console.log("Servidor rodando na porta "+port);
 });
 
