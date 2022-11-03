d588640f424cb40ff07ae92426552a907d694071
Rafael Dosso <rafael.dosso@gmail.com>
2022-11-03 11:20:19 -0300

    Configuracao do CORS

--- src/server.js
+++ src/server.js
@@ -4,8 +4,26 @@ const routes = require('./routes');
 require('./database');
 
 const app = express();
+//Cors Configuration - Start
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+        res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+        )
+        return res.status(200).json({})
+    }
+    next()
+})
+  //Cors Configuration - End
 app.use(express.json())
 app.use(routes);
+
 
 const port = process.env.PORT || 3333;
 app.listen(port, () => {
