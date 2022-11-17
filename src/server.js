d1d3f832b0ede4e9476be0b322c1a0f7be37042e
Rafael Dosso <rafael.dosso@gmail.com>
2022-11-17 12:10:30 -0300

    Liberando * CORS

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 const port = process.env.PORT || 3333;
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:' + port);
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
