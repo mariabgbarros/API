f7453480a6a41aa392ebe3a42e9c559ccaf08a7c
Rafael Dosso <rafael.dosso@gmail.com>
2022-11-17 11:37:50 -0300

    Adicionando calculo do consumo

--- src/controllers/RefeicaoController.js
+++ src/controllers/RefeicaoController.js
@@ -1,7 +1,11 @@
 const Usuario = require('../models/Usuario');
 const Refeicao = require('../models/Refeicao');
+const Alimento = require('../models/Alimento');
+
+const axios = require('axios')
 
 module.exports = {
+
     async index(req, res) {
         const { id } = req.params;
 
@@ -23,6 +27,10 @@ module.exports = {
                 }
             }
         });
+
+        if (!usuario) {
+            return res.status(400).json({error: 'Usuario nao encontrado'});
+        }
 
         return res.json(usuario.refeicoes);
     },
@@ -55,5 +63,53 @@ module.exports = {
         await refeicao.destroy();
 
         return res.json();
+    },
+
+    async getConsumo(req, res) {
+        const { id } = req.params;
+
+        const refeicao = await Refeicao.findByPk(id, {
+            include: {
+                association: 'alimentos',
+                attributes: ['nome','qtd_g'],
+            }
+        });
+
+        if (!refeicao) {
+            return res.status(400).json({erro:"Refeicao nao encontrado"})
+        }
+
+        let qtd_cal = 0;
+        let qtd_prot = 0;
+        let qtd_lip = 0;
+        let qtd_carb = 0;
+
+        const options = {
+            method: 'GET',
+            url: 'https://calorieninjas.p.rapidapi.com/v1/nutrition',
+            params: {query: ''},
+            headers: {
+              'X-RapidAPI-Key': '5c78918395mshc73ddfde387d928p19462bjsn7c46636e89dc',
+              'X-RapidAPI-Host': 'calorieninjas.p.rapidapi.com'
+            }
+        };
+
+        for (const alimento of refeicao.alimentos) {
+            options.params = {query: alimento.nome};
+            axios.request(options).then(function (response) {
+
+                qtd_cal += response.data.items[0].calories * alimento.qtd_g / 100;
+                qtd_prot += response.data.items[0].protein_g * alimento.qtd_g / 100;
+                qtd_carb += response.data.items[0].carbohydrates_total_g * alimento.qtd_g / 100;
+                qtd_lip += response.data.items[0].fat_total_g * alimento.qtd_g / 100;
+
+            }).catch(function (error) {
+                console.error(error);
+            });
+
+            console.log(qtd_cal);
+        }
+
+        return res.json({ qtd_cal, qtd_prot, qtd_lip, qtd_carb });
     }
 }
