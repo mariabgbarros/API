3fdcdcabd0e2b7ab2b3ae0465abf0b86dab0b8b8
Rafael Dosso <rafael.dosso@gmail.com>
2022-11-22 08:09:11 -0300

    Setando headers CORS

--- src/server.js
+++ src/server.js
@@ -9,10 +9,18 @@ const app = express();
 app.use(express.json())
 app.use(cors());
 
+const port = process.env.PORT || 3333;
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.use(routes);
 
-const port = process.env.PORT || 3333;
 app.listen(port, () => {
     console.log("Servidor rodando na porta " + port);
 });
