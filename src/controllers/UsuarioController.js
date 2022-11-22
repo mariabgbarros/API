ac75a89f0f0c66241a4563b714aa3447cb7d80f2
Rafael Dosso <rafael.dosso@gmail.com>
2022-11-22 08:24:20 -0300

    Revert "Install CORS Yarn"
    This reverts commit 400228bbeb41e9173c8975a5b3b49b1b5b57034b.

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -55,6 +55,9 @@ module.exports = {
             altura,
             sexo,
         });
+
+        // res.setHeader('Access-Control-Allow-Origin', 'http://0.0.0.0');
+        // res.setHeader('Access-Control-Allow-Credentials', true);
         
         return res.json(usuario);
     },
